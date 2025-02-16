45d4b7119b29c44c910f7a20b5570d2c7c0ab9eb
Bryan Salonga <winstonbryan01@gmail.com>
2025-02-16 19:51:10 +0800

    Setup supabase auth

---
+++ src/actions/auth.ts
@@ -0,0 +1,8 @@
+"use server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+import { createClient } from "@/utils/supabase/server";
+import { headers } from "next/headers";
+
+//your actions here.
